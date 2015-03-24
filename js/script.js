e231d2dee3993fb8b6b80d0ca54cf08e7f95f377
bkleinbaum <benkleinbaum@Bens-MacBook-Pro.local>
2015-03-23 20:13:49 -0400

    Done!

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$("#lobster").hide();
+$("#sun").hide();  
+
+$("#red").click(function(){
+   $("#box").css("background-color", "red");
+   $("#lobster").show();
+   $("#sun").hide();  
+  
+});
+
+$("#yellow").click(function(){
+   $("#box").css("background-color", "yellow");
+    $("#sun").show();  
+    $("#lobster").hide();
+    
+    
+});
+
